2883d4e8a58efacbd00c104429818388e8c3a318
Mael <mael.thomas@data.gouv.fr>
2020-02-06 18:26:42 +0100

    Pas de pre-rendering
    C'est couteux en temps de compilation, maintenance

--- source/webpack.prod.js
+++ source/webpack.prod.js
@@ -5,45 +5,8 @@ const {
 	default: common
 } = require('./webpack.common.js')
 
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
-
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
-const path = require('path')
-const cheerio = require('cheerio')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
-const prerenderConfig = () => ({
-	staticDir: path.resolve('dist'),
-	renderer: new Renderer({
-		renderAfterTime: 5000,
-		skipThirdPartyRequests: true
-	}),
-	postProcess: context => {
-		const $ = cheerio.load(context.html)
-		// force https on twitter emoji cdn
-		$('img[src^="http://twemoji.maxcdn.com"]').each((i, el) => {
-			$(el).attr('src', (_, path) => path.replace('http://', 'https://'))
-		})
-		// Remove loader
-		$('#outdated-browser').after(`
-			<style>
-				#js {
-					opacity: 1;
-					transform: translateY(0px);
-				}
-				#loading {
-					display: none;
-				}
-			</style>
-		`)
-		// Remove piwik script
-		$('script[src$="stats.data.gouv.fr/piwik.js"]').remove()
-
-		context.html = $.html()
-		return context
-	}
-})
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
 	...common,
@@ -52,16 +15,12 @@ module.exports = {
 	},
 	output: {
 		...common.output,
-		filename: ({ chunk }) => {
-			return chunk.name === 'simulateur-iframe-integration'
-				? '[name].js'
-				: '[name].[contenthash].bundle.js'
-		}
+		filename: '[name].[contenthash].bundle.js'
 	},
 	mode: 'production',
 	devtool: 'source-map',
 	plugins: [
-		...common.plugins || [],
+		...(common.plugins || []),
 		...HTMLPlugins({ injectTrackingScript: true }),
 		new WorkboxPlugin.GenerateSW({
 			clientsClaim: true,
@@ -101,22 +60,6 @@ module.exports = {
 			// both options are optional
 			filename: '[name].[hash].css',
 			chunkFilename: '[id].[hash].css'
-		}),
-		new PrerenderSPAPlugin({
-			...prerenderConfig(),
-			outputDir: path.resolve('dist', 'prerender', 'infrance'),
-			routes: [
-				'/',
-				'/social-security/salaried',
-				'/iframes/simulateur-embauche'
-			],
-			indexPath: path.resolve('dist', 'infrance.html')
-		}),
-		new PrerenderSPAPlugin({
-			...prerenderConfig(),
-			outputDir: path.resolve('dist', 'prerender', 'mon-entreprise'),
-			routes: ['/', '/simulateurs/salarié', '/iframes/simulateur-embauche'],
-			indexPath: path.resolve('dist', 'mon-entreprise.html')
 		})
 	]
 }
